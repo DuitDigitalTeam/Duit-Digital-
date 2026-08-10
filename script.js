2426b2751c10bdebd6746b07f38f5a069f4b06d3
Duit Digital (DUIT) <duitdigitalmlm@gmail.com>
2026-08-11 02:18:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,26 @@ behavior:"smooth"
 });
 
 
+window.addEventListener("scroll",()=>{
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let position=card.getBoundingClientRect().top;
+
+let screen=window.innerHeight;
+
+
+if(position < screen - 100){
+
+card.style.opacity="1";
+
+card.style.transform="translateY(0)";
+
 }
+
+});
 
 
 });
